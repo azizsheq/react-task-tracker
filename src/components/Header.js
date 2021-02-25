19516c53e5dc46212290c1e58d8feeaebff18d28
azizsheq <azizsheq@gmail.com>
2021-02-25 11:22:15 +0800

    all components updated with complete app, except any server

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import PropTypes from 'prop-types'
+import Button from './Button'
 
-const Header = () => {
+// destructuring the (props) into ({ title })
+// then no need to use props.title 
+const Header = ({ title, onAdd, showAdd }) => {
+
     return (
-        <header>
-            <h1>Task Tracker</h1>
+        <header className='header'>
+            <h1>{title}</h1>
+            <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}></Button>
         </header>
     )
 }
+
+//creating a default props named title
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+// adding a default prop type which is string
+// finally making it as required so that
+// if there is no prop types then will display error in console 
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+// to add some css using object
+// const headingStyle = {
+//     color: 'red',
+//     backgroundColor: 'black'
+// }
 
 export default Header
